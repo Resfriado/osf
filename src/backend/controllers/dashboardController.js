fdb7ea5520ce9cf84fc8a14454e5db902e746761
Resfriado <mikaelflips@gmail.com>
2025-11-18 19:09:36 -0300

    fix: github failed

---
+++ src/backend/controllers/dashboardController.js
@@ -0,0 +1,4 @@
+const { showPage } = require("../core/utils/pageController.js");
+const newPath = "../frontend/pages/admin/index.html";
+
+exports.page = showPage(newPath);
---
+++ src/backend/routes/dashboardRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/dashboardController");
+
+router.get("/dashboard", controller.page);
+
+module.exports = router;
